e0e75fced53d88840fd49b0dd5faa15e19d43415
MatePiranishvili <matepiranishvili6@gmail.com>
2025-10-19 14:35:35 +0400

    c

--- day 87/homework/homework.js
+++ day 87/homework/homework.js
@@ -17,7 +17,16 @@
 //     console.log(i)
 // }
 
-// 4. 4-დან 100-მდე გამოიტანეთ ყველა კენტი რიცხვი. ოღონდ შეამოწმეთ if/else-თი.
+// 4.
+
+// let num1 = Number(prompt('enter youre number : '))
+// let num2 =0
+// for(let i = 1 ;i <= num1;i +=1){
+//     num2+=i
+// }
+// console.log(num2)
+
+// 5. 4-დან 100-მდე გამოიტანეთ ყველა კენტი რიცხვი. ოღონდ შეამოწმეთ if/else-თი.
 
 
 // for (let i = 4;i < 100;i +=1){
@@ -25,4 +34,22 @@
 //         console.log(i)
 // }
 
-// 5. 
+
+// 6.  
+
+// let num1 = Number(prompt('enter youre number : '))
+// for(let i = 0 ;i < num1;i +=1){
+//     if(i % 2 === 0){
+//         console.log('this number is even')
+//     }else{
+//         console.log('odd')
+//     }
+// }
+
+// 7. 
+// let num1 = Number(prompt('enter youre number : '))
+// let num2 = Number(prompt('enter youre number : '))
+// let num3 = 0
+// for(let i = num1 ; i<num2;i +=1){
+//    num3+=1
+// }console.log(num3)
