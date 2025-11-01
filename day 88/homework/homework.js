c0d4e4931b28a78292066a7159b3cb2013d67a00
MatePiranishvili <matepiranishvili6@gmail.com>
2025-11-01 21:56:45 +0400

    commit

--- day 88/homework/homework.js
+++ day 88/homework/homework.js
@@ -12,14 +12,39 @@
 // 2 
 
 
-let names  = ['goga','luka','mate','shota']
+// let name = prompt('enter youre name : ')
 
-names[0]
-names[1]
-names[2]
-names[3]
-let i = 0
-while(names === 2){
-    console.log(names(i));
-    i++;
-}
+// while(name !== 'mate'){
+//   name = prompt('enter youre name : ')z
+// }
+
+// 3
+
+// let number =  [1,2,3,4,5,6,7,8,9,10]
+
+// while(number < i){
+//     console.log(number[i])
+//     i++
+// }
+
+// 4 
+
+
+// let number = Number(prompt('enter youre number : '))
+// let i = 0
+// while(i < number ){
+//     console.log(i + 2)
+//     i++
+// }
+
+// 5
+
+
+// let list1 = [1,2,3,4,5,6,7,8,9,]
+// let list2 = ['mate','ana','goga']
+// let list3 = [12<32,32>32,12354>1]
+// let listall = [list1,list2,list3]
+
+
+// 6
+// while loop-ის გამოყენებით ჩვენ ვატრიალებთ სიას იქამდე სანამ პირობა არ შესრულდება ხოლო for loop-ის დროს ვიცით უკვე რამდენჯერ უნდა შესრულდეს მოქმედება
