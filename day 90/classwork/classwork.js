f22888a6cf15f6cdce81585270f21b392e997b69
MatePiranishvili <matepiranishvili6@gmail.com>
2025-11-05 17:46:41 +0400

    commit

---
+++ day 90/classwork/classwork.js
@@ -0,0 +1,9 @@
+// მომხმარებელს შემოატანინეთ ჯგუფის სახელი სანამ არ შემოიტანს 19-20-33-ს (დრო: 5წთ)
+
+let smth = (prompt('enter youre number : '))
+
+while(smth !== '19-20-33'){
+    smth = (prompt('enter youre number : '))
+
+   
+}
---
+++ day 90/homework/homework.js
@@ -0,0 +1,96 @@
+// 1.
+
+// let list = [10,9,8,7,6,6,5,4,3,2,1]
+
+// console.log(list)
+
+
+// 2.
+
+// let name = prompt('enter youre name : ')
+
+// console.log(name[0][-1])
+
+
+
+//    --------------------------------------
+
+
+// 1.
+// let num = Number(prompt('enter youre number bro : '))
+
+// if(num % 2 === 0){
+//     console.log('even')
+// }else{
+//     console.log('odd')
+// }
+
+// 2.
+
+// let num = Number(prompt('enter youre number bro : '))
+
+// if(num % 3 === 0){
+//     console.log('სამის ჯერადია')
+// }else{
+//     console.log('არაა სამის ჯერადი')
+// }
+
+// // 3.
+
+// let num = Number(prompt('enter youre number'))
+// console.log(num * 2)
+
+// 4.
+
+// ??
+
+
+// 5.
+
+// let grup = prompt('enter youre group')
+// if(grup === '19-20-33'){
+//     console.log('you made it')
+// }else{
+//     console.log('try again')
+// }
+
+// 6.
+// let num = ['mate','goga','ana','giorgi','nika','zuka']
+
+// console.log(num)
+
+// 7.
+
+// let LongWord = ['hellomynameismate']   ---------------- კარგად დაამუღამე!
+
+// let old = ('a')   
+
+// let New = ('@')
+
+// for(i=0;i < LongWord.length; i++){
+//     LongWord[i] = LongWord[i].replace(old, New);
+// }
+
+// console.log(LongWord)
+
+
+// 8.
+
+// ?
+
+// 9 
+
+// let num = Number(prompt('enter youre number : '))
+// console.log(num ** 2)
+
+
+// 10 
+
+// let float = Number(prompt('enter youre number'))
+// if(float % 1 === 1){
+//     console.log('It aint a float')
+// }else if(float % 1 === 0){
+//     console.log('it is a float')
+// }
+
+
